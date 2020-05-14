823dc4d5fb0436ab484a371acaf9bc1ba0136598
fixonweb <contato@fixonweb.com.br>
2020-05-14 18:29:46 -0300

    0.1.2

--- fixonweb-anivesariantes.php
+++ fixonweb-anivesariantes.php
@@ -1,15 +1,27 @@
 <?php
 /**
  * Plugin Name:     FIXONWEB - Anivesariantes
- * Plugin URI:      https://github.com/fixonweb/anivesariantes
+ * Plugin URI:      https://github.com/fixonweb/fixonweb-anivesariantes
  * Description:     Cadastrar uma lista de pessoas e poder exibir quem faz aniversário no mês atual. - Plugin WordPress
  * Author:          FIXONWEB
  * Author URI:      https://github.com/fixonweb
- * Text Domain:     fixonweb-anivesariantes
+ * Text Domain:     fix158949
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         Fixonweb_Anivesariantes
+ * @package         Fix158949
  */
 
-// Your code starts here.
+defined( 'ABSPATH' ) or die();
+
+require 'plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/fixonweb/fixonweb-anivesariantes',
+	__FILE__, 
+	'fixonweb-anivesariantes/fixonweb-anivesariantes'
+);
+
+add_action('wp_enqueue_scripts', "fix158949_enqueue_scripts");
+function fix158949_enqueue_scripts(){
+    wp_enqueue_script( 'jquery-validate-min', plugin_dir_url( __FILE__ ) . '/js/jquery.validate.min.js', array( 'jquery' )  );
+}
