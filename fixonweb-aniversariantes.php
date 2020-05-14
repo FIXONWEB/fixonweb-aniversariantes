d0146b55de762f8b85b907bfb1bd8b5cc2b39af1
fixonweb <contato@fixonweb.com.br>
2020-05-14 18:44:08 -0300

    0.1.4

---
+++ fixonweb-aniversariantes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:     FIXONWEB - Aniversariantes
+ * Plugin URI:      https://github.com/fixonweb/fixonweb-aniversariantes
+ * Description:     Cadastrar uma lista de pessoas e poder exibir quem faz aniversário no mês atual. - Plugin WordPress
+ * Author:          FIXONWEB
+ * Author URI:      https://github.com/fixonweb
+ * Text Domain:     fix158949
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fix158949
+ */
+
+defined( 'ABSPATH' ) or die();
+
+require 'plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/fixonweb/fixonweb-aniversariantes',
+	__FILE__, 
+	'fixonweb-aniversariantes/fixonweb-aniversariantes'
+);
+
+add_action('wp_enqueue_scripts', "fix158949_enqueue_scripts");
+function fix158949_enqueue_scripts(){
+    wp_enqueue_script( 'jquery-validate-min', plugin_dir_url( __FILE__ ) . '/js/jquery.validate.min.js', array( 'jquery' )  );
+}
